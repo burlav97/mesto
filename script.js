3b58abfddf70f67027d816ae363a7d10237344dc
Nastya Burlutskaya <burlav97@yandex.ru>
2020-04-18 21:01:14 +0300

    4sprint second

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let popup = document.querySelector('.popup');
-let openButton = document.querySelector('.profile__button_action_edit');
-let closeButton = document.querySelector('.popup__button_action_close');
+const popup = document.querySelector('.popup');
+const openButton = document.querySelector('.profile__button-edit');
+const closeButton = document.querySelector('.popup__button-close');
 
 let userName = document.querySelector('.profile__title');
 let userJob = document.querySelector('.profile__subtitle');
@@ -15,10 +15,8 @@ function popupOpen() {
 }
 function popupClose() {
     popup.classList.remove('popup_opened');
- 
+
 }
-openButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose);
 
 // Находим форму в DOM
 let formElement = document.querySelector('.popup__container');
@@ -30,10 +28,7 @@ function formSubmitHandler (evt) {
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
 
-    // Находим поля формы в DOM
-    let nameInput = document.querySelector('.popup__item_el_name');
-    let jobInput = document.querySelector('.popup__item_el_about-yourself');
-    
+
     userName.textContent = nameInput.value;
     userJob.textContent = jobInput.value;
 
@@ -43,3 +38,6 @@ function formSubmitHandler (evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+openButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+
